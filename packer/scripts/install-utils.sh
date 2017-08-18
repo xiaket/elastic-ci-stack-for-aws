0e011c30ce731d625a8d007a9590bfbc7b13d029
Lachlan Donald <lachlan@ljd.cc>
2017-08-18 14:20:21 +1000

    Show aws-cli versions when installing

--- packer/scripts/install-utils.sh
+++ packer/scripts/install-utils.sh
@@ -3,6 +3,8 @@ set -eu -o pipefail
 
 echo "Updating awscli..."
 sudo pip install --upgrade --user awscli
+aws --version
+sudo -H -u buildkite-agent aws --version 
 
 echo "Installing zip utils..."
 sudo yum update -y -q
